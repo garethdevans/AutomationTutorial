aec950c064929f00402d21c0b683a8ad33037448
= <email.garethevans@gmail.com>
2011-12-20 21:37:35 +1300

    Sycn conifg

--- answers/config/config_dev.rb
+++ answers/config/config_dev.rb
@@ -1,6 +1,5 @@
 $config.merge!(
 {
-"google_search_page_location" => "http://www.google.co.nz",
-"bing_search_page_location" => "http://nz.bing.com"
+"google_search_page_location" => "http://www.google.co.nz", "bing_search_page_location" => "http://nz.bing.com"
 }
 )
