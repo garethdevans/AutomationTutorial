9c96b4932f56f4fca370f86f57b57d823f45c1dc
toddb <todd@goneopen.com>
2012-03-01 15:58:26 +1300

    Add support for webdriver in .net

--- concordion.net/src/concordion.spec/Google/AssuritySearchTest.cs
+++ concordion.net/src/concordion.spec/Google/AssuritySearchTest.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using Concordion.Integration;
+using Concordion.Spec.Provider;
 
 namespace Concordion.Spec.Google
 {
     [ConcordionTest]
-    public class AssuritySearchTest
+    public class AssuritySearchTest : DriverFixture
     {
+
         public String ResultFor(string provider, string keywords)
         {
-            return "Agile Testing Training Courses in Auckland New Zealand";
+            var searchPage = new GoogleSearchPage(WebDriver);
+            var resultsPage = searchPage.SearchFor(keywords);
+            return resultsPage.Results();
         }
     }
 }
---
+++ concordion.net/src/concordion.spec/Provider/GoogleSearchPage.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Code ported from java from https://bitbucket.org/twoten/opensourcetesting
+ * todd@goneopen.com 2012
+ */
+using OpenQA.Selenium;
+
+namespace Concordion.Spec.Provider
+{
+    public class GoogleSearchPage
+    {
+        private readonly IWebDriver _webDriver;
+        private readonly By _queryBox = By.Name("q");
+
+        public GoogleSearchPage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+            webDriver.Navigate().GoToUrl("http://google.com");
+        }
+
+        public GoogleResultsPage SearchFor(string queryString)
+        {
+            _webDriver.FindElement(_queryBox).SendKeys(queryString);
+            return new GoogleResultsPage(_webDriver);
+        }
+    }
+}
